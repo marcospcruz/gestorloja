7f38c32f74b3432134907610bf6192699abb9480
marcospcruz <marcpcruz@gmail.com>
2018-07-24 18:24:31 -0300

    mudando script commit

--- commit.bat
+++ commit.bat
@@ -8,11 +8,15 @@ call mvn eclipse:clean
 rem * * * sleep 5
 ping 127.0.0.1 -n 6 > nul
 
+git checkout fx_release
+
+git branch
+
 git add --all .
 
 git commit -m %mensagemCommit%
 
-rem * * * git push origin master
-git push https://github.com/marcospcruz/gestorloja.git
+git push origin fx_release
+rem * * * git push https://github.com/marcospcruz/gestorloja.git
 
 call mvn eclipse:eclipse
